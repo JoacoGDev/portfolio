df5e9ce976fea403ec7f3866becd32df9004543a
JG <joacogilp@gmail.com>
2025-09-23 22:37:46 -0300

    Se modifican datos de proyectos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,6 @@
 import foto from "../assets/mockup 1.jpg"
+import foto2 from "../assets/681shots_so.webp"
+import foto3 from "../assets/32shots_so.webp"
 
 export type Project = {
     title: string;
@@ -19,11 +21,19 @@ export const projects: Project[] = [
         imageUrl: foto,
     },
      {
-        title: "QrCode Generator",
-        description: "A web application that generates QR codes for URLs.",
-        technologies: ["React", "Node.js", "Express", "Tailwind CSS", "QRCode"],
+        title: "Pollomorfismo",
+        description: "Un sistema de gestión de restaurantes para clientes y gestores",
+        technologies: ["Java", "Swing"],
         githubUrl: "https://github.com/JoacoGDev/realtime-chat",
         demoUrl: "https://qr-code-generator-ejuj.onrender.com",
-        imageUrl: foto,
+        imageUrl: foto2,
+    },
+     {
+        title: "Realtime Chat",
+        description: "A chat application that allows users to communicate in real-time using WebSockets.",
+        technologies: ["Node.Js", "Express", "JWT", "SQL", "Socket.io"],
+        githubUrl: "https://github.com/JoacoGDev/realtime-chat",
+        demoUrl: "https://lavish-curiosity-production-f194.up.railway.app/chat.html",
+        imageUrl: foto3,
     }
 ]
